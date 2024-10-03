4fab5d872117ce6a02d4a8a8684214bb102aff50
Daniel Zhu <danielfangzhu@gmail.com>
2024-10-02 21:45:07 -0700

    will this work??

--- app/texts/[slug]/page.tsx
+++ app/texts/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { Text } from '@/components/text';
-import { promises as fs } from 'fs';
+import fs from 'fs';
+
 export default async function Page({ params }: { params: { slug: string } }) {
-  const text = await fs.readFile(process.cwd() + `/app/texts/${params.slug}.md`, 'utf8');
+  const text = fs.readFileSync(process.cwd() + `/app/texts/${params.slug}.md`, 'utf8');
   return <Text text={text} />
 }
